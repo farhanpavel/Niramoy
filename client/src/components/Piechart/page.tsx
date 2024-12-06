2eda092448db9427a2ea6c56d40b467014236217
Zunaid Ali <zpokean@gmail.com>
2024-12-06 15:45:56 +0600

    Minor text changes in patient dashboard

--- client/src/components/Piechart/page.tsx
+++ client/src/components/Piechart/page.tsx
@@ -58,10 +58,10 @@ export function Piecharts() {
   }, []);
 
   return (
-    <Card className="flex flex-col">
+    <Card className="flex flex-col font-tiro">
       <CardHeader className="items-center pb-0">
-        <CardTitle>Pie Chart - Donut with Text</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
+        <CardTitle>মুড চার্ট</CardTitle>
+        <CardDescription>৬ নভেম্বর - ৬ ডিসেম্বর</CardDescription>
       </CardHeader>
       <CardContent className="flex-1 pb-0">
         <ChartContainer
@@ -102,7 +102,7 @@ export function Piecharts() {
                           y={(viewBox.cy || 0) + 24}
                           className="fill-muted-foreground"
                         >
-                          Visitors
+                          দিন
                         </tspan>
                       </text>
                     );
@@ -115,10 +115,10 @@ export function Piecharts() {
       </CardContent>
       <CardFooter className="flex-col gap-2 text-sm">
         <div className="flex items-center gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+          বেশিরভাগ সময় আপনি বিষণ্ণ ছিলেন <TrendingUp className="h-4 w-4" />
         </div>
         <div className="leading-none text-muted-foreground">
-          Showing total visitors for the last 6 months
+          আপনার মুড ভালো করতে আমার সাথে কথা বলতে পারেন
         </div>
       </CardFooter>
     </Card>
